3527d296f4b6b141b7d782c9c1962f54f52b50cb
tranductrinh <trinh.tran@smartdev.vn>
2015-12-30 10:31:59 +0700

    Fix get auditor - spring auditing Fix version no

--- src/main/java/ch/smartlinksa/intern/dao/entity/AuditorAwareImpl.java
+++ src/main/java/ch/smartlinksa/intern/dao/entity/AuditorAwareImpl.java
@@ -1,12 +1,23 @@
 package ch.smartlinksa.intern.dao.entity;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuditorAwareImpl implements AuditorAware<String>{
+
+    public static final String ANONYMOUS_USER = "anonymousUser";
+
     @Override
     public String getCurrentAuditor() {
-        return "Lien_default";
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && !ANONYMOUS_USER.equals(auth.getPrincipal())) {
+            User user = (User) auth.getDetails();
+            return user.getId();
+        }
+        return "SYSTEM";
     }
+
 }
