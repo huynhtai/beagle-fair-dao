b21de8c102f8526b9539d50d353754066594ee95
PhoPhaoHoa <mailien.bk@gmail.com>
2015-12-30 10:32:06 +0700

    Create transaction entity and changelog

---
+++ src/main/java/ch/smartlinksa/intern/dao/entity/Transaction.java
@@ -0,0 +1,92 @@
+package ch.smartlinksa.intern.dao.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="transaction")
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name="type_of_transaction", discriminatorType=DiscriminatorType.INTEGER)
+public class Transaction extends AbstractAuditableEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name="transaction_id")
+    private String transactionId;
+
+    @Column(name="product_code")
+    private String productCode;
+
+    private int quantity;
+
+    @Column(name="result_code")
+    private String resultCode;
+
+    @Column(name="result_message")
+    private String resultMessage;
+
+    @Column(name="unit_price")
+    private double unitPrice;
+
+    @Column(name="user_id")
+    private String userId;
+
+    public Transaction() {
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public String getResultMessage() {
+        return resultMessage;
+    }
+
+    public void setResultMessage(String resultMessage) {
+        this.resultMessage = resultMessage;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
