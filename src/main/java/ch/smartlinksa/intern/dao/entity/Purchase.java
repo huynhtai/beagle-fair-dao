b7dcd96c35a373967ddb251df09346ffbf82d9b4
huynhtai <taihuynhbk@gmail.com>
2015-12-30 13:46:06 +0700

    add Entity Purchase.java, add changelog-1.3

---
+++ src/main/java/ch/smartlinksa/intern/dao/entity/Purchase.java
@@ -0,0 +1,47 @@
+package ch.smartlinksa.intern.dao.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@Table(name="purchase")
+@DiscriminatorValue("2")
+public class Purchase implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String address;
+
+	private String id;
+
+	@Column(name="total_price")
+	private double totalPrice;
+
+	public Purchase() {
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getTotalPrice() {
+		return this.totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+}
